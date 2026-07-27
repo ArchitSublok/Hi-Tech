715da3cdd2e7acf92853e88b33b045f11e3faf10
Archit Sublok <architsublok@gmail.com>
2026-07-28 00:43:07 +0530

    made the product page

--- Hi_Tech/products/urls.py
+++ Hi_Tech/products/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('home_page.urls')),
-    path('cart',include('cart.urls')),
-    path('products/',include('products.urls')),
+    path('', views.products,name='products'),
 ]
