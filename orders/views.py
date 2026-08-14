9cc14ef92287872977a8c43f52bce88b96582ea7
Archit Sublok <architsublok@gmail.com>
2026-08-14 14:57:10 +0530

    now will make changes in html css

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def order_list(request):
+    return render(request, 'orders/order_list.html')
