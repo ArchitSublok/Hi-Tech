86aa119bbe890b532fe3157cae637d7d3bbfab89
Archit Sublok <architsublok@gmail.com>
2026-08-15 21:57:54 +0530

    changed the whole styling of the website

--- home_page/views.py
+++ home_page/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from products.models import Product
 
-# Create your views here.
 def home(request):
-    return render(request, 'home_page/home.html')
-
+    featured_products = Product.objects.all()[:4]
+    return render(request, 'home_page/home.html', {'featured_products': featured_products})
--- products/views.py
+++ products/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Product, Category
 
-# Create your views here.
 def products(request):
-    return render(request, 'products/products.html')
+    queryset = Product.objects.select_related('category').all()
 
+    category_id = request.GET.get('category')
+    if category_id:
+        queryset = queryset.filter(category_id=category_id)
+
+    sort = request.GET.get('sort')
+    if sort == 'price_asc':
+        queryset = queryset.order_by('price')
+    elif sort == 'price_desc':
+        queryset = queryset.order_by('-price')
+    else:
+        queryset = queryset.order_by('-created_at')
+
+    paginator = Paginator(queryset, 12)
+    page_obj = paginator.get_page(request.GET.get('page'))
+
+    return render(request, 'products/products.html', {
+        'products': page_obj,
+        'categories': Category.objects.all(),
+        'selected_category': category_id,
+        'selected_sort': sort,
+    })
