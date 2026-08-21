52b73880299d1dc8113e6f1b25e3a43d8affb44e
Archit Sublok <architsublok@gmail.com>
2026-08-21 11:28:43 +0530

    change

--- Hi_Tech/settings.py
+++ Hi_Tech/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('2fV9nQ7wL3xY8mK5rT1cH6pA4sD0uJ9bE2gF7vN3qW8zX5kM1hR6tC0yP4dS9aB')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
