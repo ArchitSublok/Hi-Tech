4254db4a7c57f3eae4938b4008978238ee78e698
ArchitSublok <architsublok@gmail.com>
2026-09-01 15:41:45 +0530

    Add cloudinary to installed apps

--- Hi_Tech/settings.py
+++ Hi_Tech/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
     'home_page',
     'products',
     'about_us',
